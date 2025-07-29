1943e17e98e22ab25b66a7ae568e7cb08399e948
Jose Castro <imma.castro27@gmail.com>
2025-07-29 00:14:14 -0600

    Updating basic commands and a dummy file

--- Basic_commands.sh
+++ Basic_commands.sh
@@ -1,5 +1,19 @@
-# Comandos basicos
+# ------- Comandos basicos
 git --version
 
-# Configurar git
-git config --global
+# ------- Configurar git user
+git config --global user.name "Jose Castro"
+git config --global user.email imma.castro27@gmail.com
+# Configurar el editor de texto por defecto "code" : Visual Studio Code
+git config --global core.editor "code --wait"
+# Abrir archivo de configuracion
+git config --global -e
+# Configurar caracteres para saltos de linea CR LF
+# Windows: true, Linux/Mac: input
+git config --global core.autocrlf true
+# Configuraciones
+git congif -h
+
+# ------ Basic commands
+# Start using the repository
+git init
